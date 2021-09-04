eb67c8515b2a61f9209b44dc720f942042b3dddf
CesarBoria <88832273+CesarBoria@users.noreply.github.com>
2021-09-04 17:55:12 +0200

    Uploading a file

---
+++ Anton and polyhedrons.py
@@ -0,0 +1,13 @@
+polyhedrons = {'Tetrahedron': 4, 'Cube': 6, 'Octahedron': 8, 'Dodecahedron': 12, 'Icosahedron': 20}
+
+number_polyhedrons = int(input())
+
+list_polyhedrons = []
+for i in range(number_polyhedrons):
+    list_polyhedrons.append(input())
+
+faces = 0
+for i in range(len(list_polyhedrons)):
+    faces += polyhedrons[list_polyhedrons[i]]
+
+print(faces)
